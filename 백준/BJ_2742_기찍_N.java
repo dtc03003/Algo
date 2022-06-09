22be4f85d12fae807a3afe0b11b7f4e418667e56
김동현 <dtc03003@naver.com>
2022-06-10 03:30:21 +0900

    BJ_2741, 2742

---
+++ 백준/BJ_2742_기찍_N.java
@@ -0,0 +1,17 @@
+package 백준;
+// 기찍 N
+// https://www.acmicpc.net/problem/2742
+
+import java.util.Scanner;
+
+public class BJ_2742_기찍_N {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
