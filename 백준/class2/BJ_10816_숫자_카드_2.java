b703531f1f2a93f6e5ea3e711404b782c318e62e
김동현 <dtc03003@naver.com>
2022-06-26 22:04:38 +0900

    22.06.26

---
+++ 백준/class2/BJ_10816_숫자_카드_2.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_10816_숫자_카드_2 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
