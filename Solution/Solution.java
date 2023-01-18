ab68badbdee25e4e455186785efa89f2e82561f1
김동현 <dtc03003@naver.com>
2023-01-18 23:11:59 +0900

    23.01.18

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3,20 +3,17 @@ import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
-        System.out.println("최종 "+solution("hello", "hello"));
+        System.out.println("최종 "+solution());
     }
     
-    public static int solution(String before, String after) {
-        int answer = 1;
-        String[] be = before.split("");
-        String[] af = after.split("");
+    public static int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        int num = 0;
 
-        Arrays.sort(be);
-        Arrays.sort(af);
-
-        for (int i = 0; i < af.length; i++) {
-            if (!be[i].equals(af[i]))
-                answer = 0;
+        for (int i = 0; i < num_list.length / n; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = num_list[num++];
+            }
         }
 
         return answer;
