7b510d7fa3e19e8f4ed15670e74868b83d37f1b4
김동현 <dtc03003@naver.com>
2023-01-22 22:56:11 +0900

    23.01.22

---
+++ baekjoon/다시풀어보기/P2018_연속된자연수의합.java
@@ -0,0 +1,27 @@
+package 다시풀어보기;
+import java.util.Scanner;
+
+public class P2018_연속된자연수의합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int start_index = 1;
+        int end_index = 1;
+        int cnt = 1;
+        int sum = 1;
+
+        while (N != end_index) {
+            if (N == sum) {
+                cnt++;
+                sum += ++end_index;
+            } else if (N < sum) {
+                sum -= start_index++;
+            } else {
+                sum += ++end_index;
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+}
