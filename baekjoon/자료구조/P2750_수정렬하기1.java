4eaa7cdfaee1f8cf01c4903264b3ffef5e8a6ef9
김동현 <dtc03003@naver.com>
2023-01-31 23:40:27 +0900

    23.01.31

---
+++ baekjoon/자료구조/P2750_수정렬하기1.java
@@ -0,0 +1,25 @@
+package 자료구조;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2750_수정렬하기1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] num = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        for (int i : num) {
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
