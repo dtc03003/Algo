ecf7d631a9332eb7520d602702a11874b756a1de
김동현 <dtc03003@naver.com>
2023-02-15 23:05:44 +0900

    23.2.15

---
+++ baekjoon/그리디/P1744_수묶기.java
@@ -0,0 +1,60 @@
+package 그리디;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class P1744_수묶기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> plusQue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusQue = new PriorityQueue<>();
+
+        int zero = 0;
+        int one = 0;
+
+        for (int i = 0; i < N; i++) {
+            int data = sc.nextInt();
+            if (data > 1) {
+                plusQue.add(data);
+            } else if (data == 1) {
+                one++;
+            } else if (data == 0) {
+                zero++;
+            } else {
+                minusQue.add(data);
+            }
+        }
+
+        int sum = 0;
+
+        // 양수
+        while (plusQue.size() > 1) {
+            int first = plusQue.remove();
+            int second = plusQue.remove();
+            sum += first * second;
+        }
+
+        if (!plusQue.isEmpty()) {
+            sum += plusQue.remove();
+        }
+
+        // 음수
+        while (minusQue.size() > 1) {
+            int first = minusQue.remove();
+            int second = minusQue.remove();
+            sum += first * second;
+        }
+
+        if (!minusQue.isEmpty()) {
+            if (zero == 0) {
+                sum += minusQue.remove();
+            }
+        }
+
+        sum = sum + one;
+        System.out.println(sum);
+    }
+}
--- baekjoon/탐색/p11724_연결요소의개수.java
+++ baekjoon/탐색/p11724_연결요소의개수.java
@@ -1,4 +1,4 @@
-package 정렬;
+package 탐색;
 
 import java.io.BufferedReader;
 import java.io.IOException;
