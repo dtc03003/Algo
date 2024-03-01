ac76c5026d159dbdc7adb04bf2a6de7170c1840e
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-03-01 19:09:50 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,10 +1,3 @@
 def solution(dots):
-    answer = 0
-    dots = sorted(dots)
-    if ((dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0])) == ((dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0])):
-        answer = 1
-    else:
-        if (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]) == (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]):
-            answer = 1
-
-    return answer
+    dots.sort()
+    return 1 if ((dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0])) == ((dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0])) else 1 if (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]) == (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]) else 0
