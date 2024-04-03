db3b77792dc7ba7b860b0dfa88cfbc3daf5f42c1
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-04-03 09:08:40 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -2,4 +2,4 @@ def solution(num_list):
     num = 1
     for i in num_list:
         num = num*i 
-    return 0 if num > sum([i for i in num_list])**2 else 1
+    return 0 if num > sum(num_list)**2 else 1
