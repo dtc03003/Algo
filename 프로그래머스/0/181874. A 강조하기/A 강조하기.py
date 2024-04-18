6fcd333cd7b828d922a590655534190265e6298b
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-04-19 01:25:34 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,11 @@
+def solution(myString):
+    answer = ''
+    
+    for i in myString.lower():
+        if i == 'a':
+            answer += "A"
+        else:
+            answer += i
+
+            
+    return answer
