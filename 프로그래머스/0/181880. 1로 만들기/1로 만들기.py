854d17bc5b1ab04de03d32893b4146f1cea89358
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-05-23 00:25:03 +0900

    [level 0] Title: 1로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -4,10 +4,7 @@ def solution(num_list):
         while True:
             if i == 1:
                 break;
-            if i % 2 == 0:
-                i = i // 2;
-                answer += 1;
-            else:
-                i = (i - 1) // 2
-                answer += 1;
+            i = i // 2;
+            answer += 1;
+
     return answer
