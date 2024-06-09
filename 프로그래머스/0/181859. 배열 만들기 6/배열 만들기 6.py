e79732662f7fce0f87d7c6efc238657a7e4d62cc
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-06-09 16:07:25 +0900

    [level 0] Title: 배열 만들기 6, Time: 20.55 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,12 +1,9 @@
 def solution(arr):
     stk = []
-    
     for i in range(len(arr)):
-        if(not stk):
-            stk.append(arr[i])
-        elif(stk[-1] == arr[i]):
+        if(stk and stk[-1] == arr[i]):
             stk.pop()
         else:
             stk.append(arr[i])
         i += 1
-    return [-1] if not stk else stk
+    return stk or [-1] 
