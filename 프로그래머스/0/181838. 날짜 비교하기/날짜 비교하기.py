b6bd776ab7bd93fe9e8061811141d14c5475b90f
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-07-17 23:41:58 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,9 +1,2 @@
 def solution(date1, date2):
-    if(date1 == date2):
-        return 0
-    else:
-        for i in range(3):
-            if(date1[i] < date2[i]):
-                return 1
-            elif(date1[i] > date2[i]):
-                return 0
+    return int(date1 < date2)
