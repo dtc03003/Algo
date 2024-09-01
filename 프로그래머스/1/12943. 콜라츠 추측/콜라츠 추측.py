cb92f4f37b105e5f9283343c7748e1aa24fccf03
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-09-01 14:06:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -3,9 +3,8 @@ def solution(num):
     while(not(num == 1)):
         if(index == 500):
             return -1
-        if(num % 2 == 0):
-            num = num // 2
-        else:
-            num = num * 3 + 1
+        
+        num = num // 2 if(num % 2 == 0) else num * 3 + 1
+        
         index += 1
     return index
