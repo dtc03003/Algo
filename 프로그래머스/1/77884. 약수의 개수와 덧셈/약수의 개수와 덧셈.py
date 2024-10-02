508902ef92749b17276d10101e0d7078e07494ab
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-10-02 13:28:32 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.22 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -3,6 +3,7 @@ def solution(left, right):
     for i in range(left, right+1):
         cnt = 0
         for j in range(1, int(i**(1/2))+1):  
-            cnt += 2 - (1 if j**2 == i else 0) if i % j == 0 else 0
+            if i % j == 0:
+                cnt += 2 if j**2 != i else 1
         answer += i if cnt % 2 == 0 else -i     
     return answer
