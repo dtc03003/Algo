7cc5d407b96d3802c6ee5a73d34ef9ecaae9044c
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-10-06 19:19:13 +0900

    [level 2] Title: 숫자의 표현, Time: 4.54 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -4,9 +4,7 @@ def solution(n):
         sum = 0
         for j in range(i, n+1):
             sum += j
-            print(sum)
             if sum == n:
-                print("증가")
                 answer += 1
                 break;
             elif sum > n:
