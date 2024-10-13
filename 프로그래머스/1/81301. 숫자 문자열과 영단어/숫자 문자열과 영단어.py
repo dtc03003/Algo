d12715a77ded476196cd10909b9c669975baaf4d
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-10-13 18:38:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,16 +1,7 @@
 def solution(s):
-    answer = ""
-    num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    stack = []
+    answer = s
+    num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
     
-    for i in s:
-        if not i.isnumeric():
-            stack.append(i)
-            
-            if "".join(stack) in num:
-                answer += str(num.index("".join(stack)))
-                stack.clear()
-        else:
-            answer += i
-            
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
     return int(answer)
